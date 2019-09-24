fe44ecaecb301843f5bd9a9982bf56dbf7245116
Joshus Tenakhongva <jtt75@nau.edu>
2019-09-23 20:45:09 -0700

    Most of game is done. Bat is mostly able to hit ball and based on when the bat hits the ball, the trajectory is determined

--- game.js
+++ game.js
@@ -1,26 +1,181 @@
 var gameport = document.getElementById( "gameport" );
 
-var renderer = PIXI.autoDetectRenderer({ width: 500, height: 500, backgroundColor: 0x3344ee }); 
-gameport.appendChild( renderer.view ); 
+var renderer = PIXI.autoDetectRenderer(
+												{ width: 400, height: 400, backgroundColor: 0x6ac48a });
 
-var stage = new PIXI.Container(); 
-var texture = PIXI.Texture.fromImage("Arrow.png"); 
-var sprite = new PIXI.Sprite( texture ); 
+gameport.appendChild( renderer.view );
 
-sprite.anchor.x = 0.5; 
-sprite.anchor.y = 0.5; 
+var stage = new PIXI.Container();
 
-sprite.position.x = 200; 
-sprite.position.y = 200; 
+var baseball_bat = new PIXI.Sprite( PIXI.Texture.fromImage( "bat.png" ));
+var baseball = new PIXI.Sprite( PIXI.Texture.fromImage( "ball.png" ));
+var target_1 = new PIXI.Sprite( PIXI.Texture.fromImage( "target.png" ));
 
-stage.addChild( sprite ); 
+var pitcher_idle_frame = PIXI.Texture.fromFrame( "pitcher_idle.png" );
+var pitcher_windup_frame = PIXI.Texture.fromFrame( "pitcher_windup.png" );
+var pitcher_throw_frame = PIXI.Texture.fromFrame( "pitcher_throw.png" );
 
-function animate() {
-	requestAnimationFrame( animate ); 
-	sprite.rotation += 0.1; 
-	renderer.render( stage ); 
-}
+var pitcher = new PIXI.Sprite( pitcher_idle_frame );
 
-animate(); 
+var ball_x_position;
+var ball_y_position;
+var ball_speed = 0;
+var ball_direction = 0.0;
+var target_size = 25;
+var score = 0;
+
+stage.addChild( baseball_bat );
+baseball_bat.anchor.x = 0.5;
+baseball_bat.anchor.y = 0.9;
+baseball_bat.position.x = 350;
+baseball_bat.position.y = 215;
+//baseball_bat.rotation = -36;
+
+stage.addChild( pitcher );
+pitcher.anchor.x = 0.5;
+pitcher.anchor.y = 0.5;
+pitcher.position.x = 50;
+pitcher.position.y = 200;
+
+stage.addChild( target_1 );
+target_1.anchor.x = 0.5;
+target_1.anchor.y = 0.5;
+
+//createjs.RotationPlugin.install();
 
 
+/*
+	Stub functions
+*/
+
+function pitchAnimation()
+	{
+
+
+
+	}
+
+function swingBatEventHandler( e )
+	{
+
+	if( e.keyCode == 32 && e.target == document.body )
+		{
+
+    // Make sure the space doesn't scroll the page
+		e.preventDefault();
+
+    // Save the current ball
+    ball_x_position = baseball.position.x * 1.0;
+
+    //ball_speed = -5;
+
+    // Check if the ball position is inside the hitbox
+    if( ball_x_position >= 300 &&
+        ball_x_position <= 380 &&
+        baseball.position.y == 200 )
+      {
+
+      ball_speed = 0;
+
+      // If so, then proceed to calculate the new ball direction
+      ball_direction = ( 350.0 - ball_x_position ) / 30.0;
+      ball_direction = ball_direction * 300 + 50;
+
+      createjs.Tween.get( baseball.position ).to(
+                                            {x: 0, y: ball_direction }, 1000 );
+
+      }
+
+    // Otherwise, it is a strike
+		}
+	}
+
+function pitchBallEventHandler( e )
+	{
+
+	if( e.keyCode == 66 )
+		{
+
+    // Check that the ball is live
+
+
+    // Spawn the ball
+    spawnBaseball();
+
+    // Change the ball speed to move toward the bat
+    ball_speed = 5;
+
+    pitcher.sprite( pitcher_throw_frame );
+		}
+	}
+
+function spawnBaseball()
+	{
+
+	stage.addChild( baseball );
+	baseball.anchor.x = 0.5;
+	baseball.anchor.y = 0.5;
+	baseball.position.x = 50;
+	baseball.position.y = 200;
+	}
+
+function checkBallOutsideOfPlay()
+	{
+
+	if( baseball.position.x >= 400 ||
+			baseball.position.x <= 0   ||
+      baseball.position.y >= 400 ||
+      baseball.position.y <= 0 )
+		{
+
+    ball_speed = 0;
+    ball_direction = 0.0;
+		stage.removeChild( baseball );
+
+		return true;
+		}
+
+	return false;
+	}
+
+function randomizeTargetPosition( target )
+	{
+
+	target.position.x = 25;
+	target.position.y = Math.random() * 340 + 30;
+	}
+
+function checkTargetHit( target )
+  {
+
+  if( baseball.position.x >= target.position.x - target_size &&
+      baseball.position.x <= target.position.x + target_size &&
+      baseball.position.y >= target.position.y - target_size &&
+      baseball.position.y <= target.position.y + target_size )
+    {
+
+    //
+    randomizeTargetPosition( target );
+    score++;
+    }
+  }
+
+function animate()
+	{
+
+	requestAnimationFrame( animate );
+	renderer.render( stage );
+
+  baseball.position.x += ball_speed;
+  //baseball.position.y += ball_direction;
+
+  checkBallOutsideOfPlay();
+  checkTargetHit( target_1 );
+
+  document.getElementById( "score" ).innerHTML = score;
+	}
+
+randomizeTargetPosition( target_1 );
+document.addEventListener( 'keydown', swingBatEventHandler );
+document.addEventListener( 'keydown', pitchBallEventHandler );
+animate();
